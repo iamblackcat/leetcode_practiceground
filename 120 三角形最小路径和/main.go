0a11ba47cc16f12acfc73d2b608ee1b1de987a3e
iamblackcat <iamblackcat@foxmail.com>
2024-05-19 16:45:32 +0800

    120 进阶解决

--- 120 三角形最小路径和/main.go
+++ 120 三角形最小路径和/main.go
@@ -14,28 +14,32 @@ func main() {
 
 func minimumTotal(triangle [][]int) int {
 	m := len(triangle)
-	n := len(triangle[m-1])
+
 	// 初始化dp数组
-	dp := make([][]int, len(triangle))
+	dp := make([]int, m)
 	for i := range dp {
-		dp[i] = make([]int, len(triangle[i]))
-	}
-	// 边缘状态
-	dp[0][0] = triangle[0][0]
-	for i := 1; i < len(triangle); i++ {
-		dp[i][0] = dp[i-1][0] + triangle[i][0]
+		dp[i] = 0
 	}
 	// 动态规划
-	for i := 1; i < len(triangle); i++ {
-		for j := 1; j < len(triangle[i])-1; j++ {
-			dp[i][j] = minInt(dp[i-1][j]+triangle[i][j], dp[i-1][j-1]+triangle[i][j])
+	dp[0] = triangle[0][0]
+	for i := 1; i < m; i++ {
+		for j := len(triangle[i]) - 1; j >= 0; j-- {
+
+			if j == 0 {
+				// 左侧边时
+				dp[0] += triangle[i][0]
+			} else if j == len(triangle[i])-1 {
+				// 右侧边时
+				dp[j] = triangle[i][j] + dp[j-1]
+			} else {
+				dp[j] = minInt(dp[j], dp[j-1]) + triangle[i][j]
+
+			}
 		}
-		// 处理三角形斜边
-		dp[i][len(triangle[i])-1] = dp[i-1][len(triangle[i-1])-1] + triangle[i][len(triangle[i])-1]
 	}
-	result := dp[m-1][0]
-	for i := 0; i < n; i++ {
-		result = minInt(result, dp[m-1][i])
+	result := dp[0]
+	for i := 1; i < m; i++ {
+		result = minInt(result, dp[i])
 	}
 	return result
 }
